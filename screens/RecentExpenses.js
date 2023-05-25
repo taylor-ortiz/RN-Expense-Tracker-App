3861f0f1f3ac6200db942bb9f86063d53de2709e
taylor-ortiz <ortiz.taylor@gmail.com>
2023-05-25 06:12:03 -0600

    adding expense items and creating styling for lists. Also, added a utility function for dates to show a nice format in the app

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,8 @@
 import { Text } from 'react-native'
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
 
 function RecentExpenses() {
-    return  <Text>RecentExpenses screen</Text>
+    return  <ExpensesOutput expensesPeriod="Last 7 Days"></ExpensesOutput>
 }
 
 export default RecentExpenses
